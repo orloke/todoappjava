6787cd6232f3f29b366cdcbd36f5ae3b7ff24cef
Junior Dering <juniordering@hotmail.com>
2022-11-08 19:56:41 -0400

    connection db

--- app/src/main/java/todoapp/App.java
+++ app/src/main/java/todoapp/App.java
@@ -3,12 +3,12 @@
  */
 package todoapp;
 
+import java.sql.Connection;
+import util.ConnectionFactory;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Connection c = ConnectionFactory.getConnection();
     }
 }
